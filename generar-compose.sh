1fa8256c6eb10d3fd26308ecbf1a2f9b23b06bfe
Juan Martin Nazarian <jmnazarian@gmail.com>
2026-03-22 00:57:13 -0300

    create script to generate a docker compose file

---
+++ generar-compose.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <config_file_name> <number_of_clients>"
+    exit 1
+fi
+
+OUTPUT_FILE=$1
+NUM_CLIENTS=$2
+
+# Header and Server
+cat <<EOF > "$OUTPUT_FILE"
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+EOF
+
+# Clients
+for i in $(seq 1 "$NUM_CLIENTS"); do
+    cat <<EOF >> "$OUTPUT_FILE"
+
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+EOF
+done
+
+# Network and posibly more in the future
+cat <<EOF >> "$OUTPUT_FILE"
+
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+EOF
+
+echo "Successfully generated $OUTPUT_FILE with $NUM_CLIENTS clients."
