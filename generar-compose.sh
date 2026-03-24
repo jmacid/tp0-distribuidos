8939aee416d997255b6ea2497b60a9b50ee92409
Juan Martin Nazarian <jmnazarian@gmail.com>
2026-03-24 15:11:57 -0300

    move config file out of the build

--- generar-compose.sh
+++ generar-compose.sh
@@ -21,6 +21,8 @@ services:
       - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
+    volumes:
+      - ./server/config.ini:/config.ini
 EOF
 
 # Clients
@@ -38,6 +40,8 @@ for i in $(seq 1 "$NUM_CLIENTS"); do
       - testing_net
     depends_on:
       - server
+    volumes:
+      - ./client/config.yaml:/config.yaml
 EOF
 done
 
