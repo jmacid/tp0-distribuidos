9fd3e69938750824e2dcb21c617aba7c76a264a9
Juan Martin Nazarian <jmnazarian@gmail.com>
2026-03-24 15:31:00 -0300

    delete logging config from compose generator

--- generar-compose.sh
+++ generar-compose.sh
@@ -18,7 +18,6 @@ services:
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
     volumes:
@@ -35,7 +34,6 @@ for i in $(seq 1 "$NUM_CLIENTS"); do
     entrypoint: /client
     environment:
       - CLI_ID=$i
-      - CLI_LOG_LEVEL=DEBUG
     networks:
       - testing_net
     depends_on:
