ee3c332faf04c54a9fd33e377beb3b732058fcb7
Juan Martin Nazarian <jmnazarian@gmail.com>
2026-03-25 23:56:07 -0300

    change threads to processes

--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,6 @@
 import socket
 import logging
-import threading
+import multiprocessing
 import os
 from .utils import Bet, store_bets, load_bets, has_won
 
@@ -10,21 +10,33 @@ class Server:
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
         
-        self.lock = threading.Lock()
-        self.draw_condition = threading.Condition(self.lock)
-        self.agencies_finished = set()
+        self.manager = multiprocessing.Manager()
+        self.lock = multiprocessing.Lock()
+        self.draw_condition = multiprocessing.Condition(self.lock)
         
-        clients_env = os.environ.get('CLIENT_AMOUNT', os.environ.get('CLIENTS', 5))
-        self.expected_agencies = int(clients_env)
+        self.agencies_finished = self.manager.list()
+        self.winners_by_agency = self.manager.dict()
         
-        self.draw_done = False
-        self.winners_by_agency = {}
+        self.draw_done = multiprocessing.Value('b', False)
+        
+        amount = 0
+        for i in range(1, 15):
+            try:
+                socket.gethostbyname(f'client{i}')
+                amount += 1
+            except socket.error:
+                pass
+        
+        self.expected_agencies = amount if amount > 0 else 5
+        logging.info(f"action: detect_agencies | result: success | expected: {self.expected_agencies}")
     
     def run(self):
         while True:
             client_sock = self.__accept_new_connection()
-            t = threading.Thread(target=self.__handle_client_connection, args=(client_sock,))
-            t.start()
+            p = multiprocessing.Process(target=self.__handle_client_connection, args=(client_sock,))
+            p.start()
+            
+            client_sock.close()
 
     def __handle_client_connection(self, client_sock):
         try:
@@ -65,7 +77,7 @@ class Server:
 
         except Exception as e:
             cantidad = len(batch_bets) if not is_fin else 0
-            logging.error(f"action: apuesta_recibida | result: fail | cantidad: {cantidad}")
+            logging.error(f"action: apuesta_recibida | result: fail | cantidad: {cantidad} | error: {e}")
             try:
                 client_sock.sendall(b"ERR\n")
             except:
@@ -75,28 +87,33 @@ class Server:
 
     def __handle_fin_agencia(self, client_sock, agency_id):
         with self.draw_condition:
-            self.agencies_finished.add(int(agency_id))
+            agency_id_int = int(agency_id)
+            if agency_id_int not in self.agencies_finished:
+                self.agencies_finished.append(agency_id_int)
             
-            while len(self.agencies_finished) < self.expected_agencies and not self.draw_done:
+            while len(self.agencies_finished) < self.expected_agencies and not self.draw_done.value:
                 self.draw_condition.wait()
             
-            if not self.draw_done:
+            if not self.draw_done.value:
                 logging.info("action: sorteo | result: success")
                 self.__do_draw()
-                self.draw_done = True
+                self.draw_done.value = True
                 self.draw_condition.notify_all()
             
-            winners = self.winners_by_agency.get(int(agency_id), [])
+            winners = self.winners_by_agency.get(agency_id_int, [])
             response = ",".join(winners) + "\n"
             client_sock.sendall(response.encode('utf-8'))
 
     def __do_draw(self):
-        self.winners_by_agency = {}
+        local_winners = {}
         for bet in load_bets():
             if has_won(bet):
-                if bet.agency not in self.winners_by_agency:
-                    self.winners_by_agency[bet.agency] = []
-                self.winners_by_agency[bet.agency].append(bet.document)
+                if bet.agency not in local_winners:
+                    local_winners[bet.agency] = []
+                local_winners[bet.agency].append(bet.document)
+                
+        for agency, docs in local_winners.items():
+            self.winners_by_agency[agency] = docs
 
     def __accept_new_connection(self):
         logging.info('action: accept_connections | result: in_progress')
